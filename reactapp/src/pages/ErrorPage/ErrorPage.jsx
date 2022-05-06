2dc25b6de6eb34697bb8c39eff786d107c96fbc3
Shubham Bharti <shubhambharti04@gmail.com>
2022-05-06 10:10:04 +0530

    Error page added

---
+++ reactapp/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import errorPage from '../../assets/error-wallpapers.jpeg'
+
+export const ErrorPage = () => {
+  return (
+    <div style={{display:'flex',justifyContent:'center'}}>
+        <img src={errorPage} alt='' />
+        
+    </div>
+  )
+}
